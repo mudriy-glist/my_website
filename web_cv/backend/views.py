53d7a16a555f86347c995e38478269bcccc690ea
Peter Kulisov <newfapman@gmail.com>
2022-06-16 14:16:17 +0100

    styles for diffferent templates created

--- web_cv/backend/views.py
+++ web_cv/backend/views.py
@@ -1,5 +1,9 @@
+import re
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     return render(request, "backend/index.html")
+
+def playground(request):
+    return render(request, "backend/playground.html")
